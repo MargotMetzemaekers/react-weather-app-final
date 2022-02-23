d92761f95417a96d425026e7605e8e6de84955e3
MargotMetzemaekers <92753046+MargotMetzemaekers@users.noreply.github.com>
2022-02-23 20:23:14 +0100

    Started HTML

--- src/Weather.js
+++ src/Weather.js
@@ -1,5 +1,14 @@
 import React from "react";
+import "./Weather.css";
 
 export default function Weather() {
-  return <div>Hello</div>;
+  return (
+    <div className="Weather">
+      <h1>New York</h1>
+      <ul>
+        <li>Wednesday 07:00</li>
+        <li>Cloudy</li>
+      </ul>
+    </div>
+  );
 }
