3b85c9534361368037d13f583763096a09747f45
MargotMetzemaekers <92753046+MargotMetzemaekers@users.noreply.github.com>
2022-02-26 15:31:08 +0100

    Update WeatherTeamperature.js

--- src/WeatherTeamperature.js
+++ src/WeatherTeamperature.js
@@ -1,10 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default function WeatherTemperature(props) {
-  return (
-    <span>
-      <span className="temperature">{Math.round(props.celcius)}</span>
-      <span className="unit">℃</span>
-    </span>
-  );
+  const [unit, setUnit] = useState("celsius");
+  if (unit === "celsius") {
+    return (
+      <span>
+        <span className="temperature">{Math.round(props.celcius)}</span>
+        <span className="unit">°C | °F</span>
+      </span>
+    );
+  } else {
+    return "F";
+  }
 }
