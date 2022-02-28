c6172a584ab5481939b80ee35675e711baec814b
MargotMetzemaekers <92753046+MargotMetzemaekers@users.noreply.github.com>
2022-02-28 17:49:19 +0100

    Update WeatherForecast.js

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -5,7 +5,7 @@ export default function WeatherForecast() {
   return (
     <div className="WeatherForecast">
       <div className="row">
-        <div className="col">Thursday</div>
+        <div className="col">Thun icon 19 10</div>
       </div>
     </div>
   );
