a85bbdea79b5962e46a1980903c86fd2ecd781cb
MargotMetzemaekers <92753046+MargotMetzemaekers@users.noreply.github.com>
2022-02-28 20:08:31 +0100

    Update WeatherForecast.js

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -1,11 +1,15 @@
 import React from "react";
 import Weather from "./Weather";
+import WeatherIcon from "./WeatherIcon";
 
 export default function WeatherForecast() {
   return (
     <div className="WeatherForecast">
       <div className="row">
-        <div className="col">Thun icon 19 10</div>
+        <div className="col">
+          Thun icon 19 10
+          <WeatherIcon code="01d" />
+        </div>
       </div>
     </div>
   );
